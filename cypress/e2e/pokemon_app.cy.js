acc368e51596e80ef959e3599ec373ca54b83e1c
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-05-27 10:04:24 +0300

    Add Cypress test for single pokemon page

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('ivysaur page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
